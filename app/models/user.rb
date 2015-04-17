260d83aa58ab81272c5792802e6e1f75c694c0f1
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-17 12:30:59 -0400

    Add Basic Validation For User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  validates :username, presence: true 
+  validates :email, presence: true 
+  validates :first_name, presence: true
+  validates :last_name, presence: true 
+  validates :password, length: { minimum: 8 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :email, uniqueness: true
+	validates :username, uniqueness: true
+
 end
