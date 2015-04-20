360ada8a3b172d86b337fcb02e636a14239d775c
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-20 11:49:49 -0400

    Add Factory Girl Factory For User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    
-  end
+  	user_new_password = "Pass3word:"
 
+    first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    username   { Faker::Internet.user_name }
+    email      { Faker::Internet.free_email }
+    password   { user_new_password }
+    password_confirmation { user_new_password }
+  end
 end
