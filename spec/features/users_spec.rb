98618da245a1628679570edc971ff3216d097e59
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-20 11:49:18 -0400

    Add Sign Up Form & Feature Test

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
-require 'capybara/rails'
 require 'capybara/rspec'
 
 RSpec.feature "Users", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe "sign up process" do
+  	it "should create a user" do
+  		visit "/signup"
+  		
+  		within "#new_user" do
+  			user_new_password = "Pass3word:"
+
+  			fill_in "First Name", with: Faker::Name.first_name
+  			fill_in "Last Name", with: Faker::Name.last_name
+  			fill_in "Username", with: Faker::Internet.user_name
+  			fill_in "Email", with: Faker::Internet.free_email
+  			fill_in "Password", with: user_new_password
+  			fill_in "Password Confirmation", with: user_new_password
+  		end
+
+  		click_button "Sign Up"
+  		expect(page).to have_content "Your account has been created"
+  	end
+  end
 end
