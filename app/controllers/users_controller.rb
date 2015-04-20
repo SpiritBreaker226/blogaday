30d6c3149eaecbeab0f208fe85a9c2d6cc73c03e
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-20 10:15:44 -0400

    Add Code In User Controller To Create Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to post_path, notice: "Your account has been created"
+    else
+      flash.now.alert = "Error Creating your account"
+      render :new
+    end
   end
 
   def edit
@@ -15,5 +24,17 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :first_name, 
+      :last_name, 
+      :username, 
+      :email, 
+      :password,
+      :password_confirmation
+    )
   end
 end
