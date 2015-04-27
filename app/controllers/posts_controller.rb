c8c0500b2a5ea3c2da02629bab456cef76494185
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-27 11:00:22 -0400

    Add All Post Action Methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,55 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
+		@post = Post.new(post_params)
+
+    if @post.save
+      redirect_to root_path, notice: "Your post has been created"
+    else
+      flash.now.alert = "Error Creating your post"
+      render :new
+    end
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
 
 	def edit
-	end
+    @post = Post.find(params[:id])
+  end
 
-	def update
-	end
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update_attributes(post_params) 
+      redirect_to edit_post_path(params[:id]), notice: "Your post has been updated"
+    else
+      flash.now alert = "Error Updating your post"
+      render :edit
+    end
+  end
 
 	def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, notice: "Your post has been deleted"
 	end
+
+  private
+  def post_params
+    params.require(:post).permit(
+      :title, 
+      :body, 
+      :user_id
+    )
+  end
 end
