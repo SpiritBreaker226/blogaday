7e574f5666f246411f1aada8262efc54be4f1c5d
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-28 11:56:34 -0400

    Add Post Feature Test For Editing

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -22,4 +22,27 @@ RSpec.feature "Posts", type: :feature do
 	  	end
   	end
   end
+
+  describe "#edit" do
+  	let!(:user) { create(:user_with_posts) }
+
+  	context "frist post" do
+  		it "responds with 200" do
+  			new_post_title = Faker::Hacker.say_something_smart
+  			
+  			login_user_post(user.username, "Pass3word:")
+
+	  		visit edit_user_post_url(user, user.post.first)
+
+	  		within ".edit_post" do
+	  			fill_in "Title", with: new_post_title
+	  		end
+
+	  		click_button "Update Post"
+	  		expect(page).to have_content "post has been updated"
+
+	  		expect(Post.find(user.post.first).title).to eq new_post_title
+  		end
+  	end
+  end
 end
