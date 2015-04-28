94f004187a0894189464d6b07fedeb43b885824a
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-28 11:56:06 -0400

    Add Factory To Have An User With Posts

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,5 +8,15 @@ FactoryGirl.define do
     email      { Faker::Internet.free_email }
     password   { user_new_password }
     password_confirmation { user_new_password }
+
+  	factory :user_with_posts do
+	  	transient do
+	  		num_posts 3
+	  	end
+
+	  	after(:create) do |user, transients|
+	  		transients.num_posts.times { create(:post, user: user) }
+	  	end
+	  end
   end
 end
