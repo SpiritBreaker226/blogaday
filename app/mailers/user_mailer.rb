2376fee15066d94340a3edc7d1e528c5f99b00dc
Jason Stathopulos <jstathopulos@gmail.com>
2015-04-29 11:53:04 -0400

    Add User Mailer Reminder To Blog, Method & Email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+	def reminder_to_blog_email(user)
+		@user = user
+		@new_post_url = new_user_post_url(@user)
+
+		mail to: @user.email, subject: "Time To Blog"
+	end
 end
