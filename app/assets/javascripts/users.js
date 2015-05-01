8abc3983d391352dfcf6cacf6e8e1f528c631634
Jason Stathopulos <jstathopulos@gmail.com>
2015-05-01 12:48:50 -0400

    Remove Daily Reminder If Disable
    Remove the Daily Reminder if it is disable in order for the user not
    to be able to select it nor shows that they cannot select anything.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function checkEnableDailyReminder(enableDailyReminder) {
+	var dailyReminder = $("#user_daily_reminder_4i");
+
+	if(enableDailyReminder.checked) {
+		dailyReminder.show();
+	} else {
+		dailyReminder.hide();
+	}	
+}
+
+$("#user_enable_daily_reminder").on("click", function() {
+	checkEnableDailyReminder(this);
+});
+
+$(document).on("ready page:load", function() {
+	checkEnableDailyReminder($("#user_enable_daily_reminder")[0]);
+});
+
+
