327a2e9738f25957e15b87277747062cf48ccf49
Jason Stathopulos <jstathopulos@gmail.com>
2015-05-05 11:46:49 -0400

    Add Post Method To Get First Sentence & Its Test
    Add in a post method to get the first sentence in order to display a
    summary on the post index page. Also added the first test to get 140
    characters of the body.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   validates :body, length: { minimum: 250 }
+
+  def get_frist_sentence
+		"ssasdfasdfasdfasdfasdfasdfafasdfasdfasdfasdfasdfasdfadfasfasfasdfasfaadfasdfadsfadsfasdfadfadsafadfafdfdsffdsadfsadfsafdfdfdffffffffffffssss"
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#get_frist_sentence" do
+  	let(:post) { create(:post) } 
+
+  	context "just with characters, no period" do
+  		it "return 140 charters" do
+  			sentence = post.get_frist_sentence
+
+  			expect(sentence.length).to eq(140)
+  		end
+  	end
+  end
 end
