f96243ef05010a244b585564c4f1182cdc1a7119
Jason Stathopulos <jstathopulos@gmail.com>
2015-05-15 12:02:01 -0400

    Fix Flash Alert Missing . After flash.now

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,7 +36,7 @@ class PostsController < ApplicationController
     if @post.update_attributes(post_params) 
       redirect_to post_path(params[:id]), notice: "Your post has been updated"
     else
-      flash.now alert = "Error Updating your post"
+      flash.now.alert = "Error Updating your post"
       render :edit
     end
   end
