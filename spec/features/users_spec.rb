690d367088bf736a158452c4878a421ff3ee3bf4
Jason Stathopulos <jstathopulos@gmail.com>
2015-05-20 10:12:49 -0400

    Add Better Description To User#create Feature Test

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -2,25 +2,27 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 RSpec.feature "Users", type: :feature do
-  describe "sign up process" do
-  	it "should create a user" do
-  		visit root_url
-      find(".main-header-navigation-wrapper-non-mobile-menu").click_link "Sign Up"
+  describe "#create" do
+    context "a user" do
+    	it "responds with 200" do
+    		visit root_url
+        find(".main-header-navigation-wrapper-non-mobile-menu").click_link "Sign Up"
 
-  		within "#new_user" do
-  			user_new_password = "Pass3word:"
+    		within "#new_user" do
+    			user_new_password = "Pass3word:"
 
-  			fill_in "First Name", with: Faker::Name.first_name
-  			fill_in "Last Name", with: Faker::Name.last_name
-  			fill_in "Username", with: Faker::Internet.user_name
-  			fill_in "Email", with: Faker::Internet.free_email
-  			fill_in "Password", with: user_new_password
-  			fill_in "Password Confirmation", with: user_new_password
-  		end
+    			fill_in "First Name", with: Faker::Name.first_name
+    			fill_in "Last Name", with: Faker::Name.last_name
+    			fill_in "Username", with: Faker::Internet.user_name
+    			fill_in "Email", with: Faker::Internet.free_email
+    			fill_in "Password", with: user_new_password
+    			fill_in "Password Confirmation", with: user_new_password
+    		end
 
-  		click_button "Sign Up"
-      expect(page.status_code).to be(200)  
-  	end
+    		click_button "Sign Up"
+        expect(page.status_code).to be(200)  
+    	end
+    end
   end
 
   describe "login process" do
@@ -48,6 +50,7 @@ RSpec.feature "Users", type: :feature do
 
 			visit root_url
 			find(".main-header-navigation-wrapper-non-mobile-menu").click_link "Logout"
+
       expect(page.status_code).to be(200)
 		end
 	end
