a8ccc6a8554df987bb3ed4c917dfdea9b9b24131
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-03 13:34:25 -0400

    Add Summary To Post Parmas In The Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -53,7 +53,8 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(
       :title, 
-      :body, 
+      :body,
+      :summary,
       :user_id
     )
   end
