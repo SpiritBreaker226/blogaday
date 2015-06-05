b74f7ee026dc5a5c745bef73f370ce8431efd963
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-05 12:46:14 -0400

    Add Create Action Method To Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,30 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.user = current_user
+    @comment.post = @post
+
+    if @comment.save
+      redirect_to @post, notice: "Comment added"
+    else
+      flash.now[:alert] = "Unable to added comment"
+      render template: "posts/show"
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(
+      :body,
+      :post_id,
+      :user_id
+    )
   end
 end
