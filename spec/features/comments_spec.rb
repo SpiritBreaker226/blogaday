891895b1f23e97a8127c209a201a498087778e6d
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-29 10:30:01 -0400

    Add Feature Test For Comments#Create

---
+++ spec/features/comments_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.feature "Comments", type: :feature do
+  subject(:click_submit) { click_button "Submit" }
+
+  describe "#create" do
+  	let(:post) { create(:post) }
+    let(:user) { create(:user) }
+    subject(:visit_post) { visit post_url(post) }
+
+  	context "with post has no comments" do
+  		it "responds with 200" do
+        login_user_post(user.username, "Pass3word:")
+
+        visit_post
+
+        within "#new_comment" do
+          fill_in "Body", with: Faker::Lorem.characters
+        end
+
+        click_submit
+
+        expect(page.status_code).to be(200)
+      end
+  	end
+  end
+end
