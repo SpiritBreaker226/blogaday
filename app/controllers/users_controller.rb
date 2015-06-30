7146963a8bd3bb37371ebb016c5b4e38d67e13db
Jason Stathopulos <jstathopulos@gmail.com>
2015-06-30 15:33:50 -0400

    Add User Posts Feed For JSON

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  load_and_authorize_resource except: [:new, :create]
+  load_and_authorize_resource except: [:new, :create, :feed]
 
   def new
     @user = User.new
@@ -39,6 +39,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  def feed
+    @posts = User.find(params[:id]).posts.order(id: :desc)
+
+    respond_to do |format|
+      format.json { render json: @posts }
+    end
   end
 
   private
