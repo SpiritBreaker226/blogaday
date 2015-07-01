f3549b0c55cf8a91d3c93b38e828f0234130017e
Jason Stathopulos <jstathopulos@gmail.com>
2015-07-01 08:37:58 -0400

    Add URL Attribute To Post Serializer
    Add a URL attribute to the post serializer because the user should be
    able to go to the original post if need be.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,11 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :title, :body, :summary, :created_at
+	include Rails.application.routes.url_helpers
+
+  attributes :title, :body, :summary, :created_at, :url
 
   belongs_to :user
+
+  def url
+  	post_url(object)
+  end
 end
