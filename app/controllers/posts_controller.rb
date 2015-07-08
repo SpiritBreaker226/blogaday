8a5ef58e1d507e592e1545e1b5322ae590e7cd6d
Jason Stathopulos <jstathopulos@gmail.com>
2015-07-08 11:45:31 -0400

    Add Where For Posts#Index When There Is Search
    Add where statement for Posts#Index when there is a search params in
    order to prepare for the AJAX search of Post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
 	def index
-		@posts = Post.display_and_order_by_publish_date.page(params[:page])
+		@posts = if params[:search] 
+      Post.display_and_order_by_publish_date.where("LOWER(title) LIKE LOWER(?)", "%#{params[:search]}%").page(params[:page])
+    else
+      Post.display_and_order_by_publish_date.page(params[:page])
+    end
 	end
 
 	def new
